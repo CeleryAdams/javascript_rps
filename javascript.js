9c3895a19da92006fd88641bf7602455726dfe8f
celery adams <sally.celery.adams@gmail.com>
2022-07-22 16:34:19 -0400

    write js outline

---
+++ javascript.js
@@ -0,0 +1,31 @@
+/*
+function named getComputerChoice - randomly returns 
+"rock", "paper", "scissors"
+*/
+
+
+/*
+function called playRound that plays a single round of rock paper scissors -
+2 parameters - "playerSelection" and "computerSelection" - 
+return a string that declares winner: "You lose! Paper beats Rock"
+*/
+
+/*
+playerSelection parameter should be case-insensitive
+*/
+
+/*
+function called game() that calls the playRound function
+to play a 5-round game that keeps score and reports a 
+winner or loser at the end - use a loop
+
+for (let i = 0; i < 5; i++) { code }
+*/
+
+/*
+display results with console.log()
+*/
+
+/*
+use prompt() to get input from user
+*/
